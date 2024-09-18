c9f9d87931b526b2e21952c1d11b4770b9681f7a
CiZ01 <ffrancy01@gmail.com>
2024-09-18 14:10:35 +0200

    fixed ssr error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxtjs/google-fonts', '@nuxtjs/tailwindcss', '@nuxt/ui', '@nuxt/icon'],
+  ssr: false,
+  modules: ['@nuxt/content', '@nuxtjs/google-fonts', '@nuxtjs/tailwindcss', '@nuxt/ui', '@nuxt/icon', '@nuxt/image'],
   routeRules: {
     '/': { prerender: true }
   },
